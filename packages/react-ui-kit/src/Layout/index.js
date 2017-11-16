c8481ab0ad9496b1c5d1daa4ecc2a3cb4574c8ee
AndyTheGiant <andy.bialystock@gmail.com>
2017-11-16 15:14:33 +0100

    refactor(react-ui-kit): updated Layout structure

--- packages/react-ui-kit/src/Layout/index.js
+++ packages/react-ui-kit/src/Layout/index.js
@@ -4,3 +4,7 @@ setGlobalStyles();
 
 export * from './Column';
 export * from './Container';
+export * from './StyledApp';
+export * from './Content';
+export * from './Header';
+export * from './Footer';
