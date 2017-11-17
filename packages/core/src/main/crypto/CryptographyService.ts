50a8112e5175ec11c0b4e3fd52cf3225172c4c00
Benny Neugebauer <bn@bennyn.de>
2017-11-17 18:02:38 +0100

    removed session payload

--- packages/core/src/main/crypto/CryptographyService.ts
+++ packages/core/src/main/crypto/CryptographyService.ts
@@ -10,11 +10,9 @@ import {
   OTRRecipients,
   UserClients,
 } from '@wireapp/api-client/dist/commonjs/conversation/';
-import SessionPayloadBundle from './SessionPayloadBundle';
 
 /**
  * CryptographyService operates the Cryptobox and manages encryption and decryption.
- * It doesn't know about the format of Wire's backend payloads.
  * @constructor
  */
 export default class CryptographyService {
@@ -39,7 +37,7 @@ export default class CryptographyService {
 
   public encrypt(typedArray: Uint8Array, preKeyBundles: UserPreKeyBundleMap): Promise<OTRRecipients> {
     const recipients: OTRRecipients = {};
-    const encryptions: Array<Promise<SessionPayloadBundle>> = [];
+    const encryptions: Array<Promise<{}>> = [];
 
     for (const userId in preKeyBundles) {
       recipients[userId] = {};
@@ -47,35 +45,39 @@ export default class CryptographyService {
         const preKeyPayload: PreKey = preKeyBundles[userId][clientId];
         const preKey: string = preKeyPayload.key;
         const sessionId: string = this.constructSessionId(userId, clientId);
-        encryptions.push(this.encryptPayloadForSession(sessionId, typedArray, preKey));
+        encryptions.push(
+          this.encryptPayloadForSession(sessionId, typedArray, preKey)
+            .then(encryptedPayload => Encoder.toBase64(encryptedPayload).asString)
+            .catch(error => '💣')
+            .then(encryptedPayload => ({sessionId, encryptedPayload}))
+        );
       }
     }
 
-    return Promise.all(encryptions).then((payloads: Array<SessionPayloadBundle>) => {
-      const recipients: OTRRecipients = {};
+    return Promise.all(encryptions)
+      .then((payloads: Array<{}>) => {
+        const recipients: OTRRecipients = {};
 
-      if (payloads) {
-        payloads.forEach((payload: SessionPayloadBundle) => {
-          const sessionId: string = payload.sessionId;
-          const encrypted: string = payload.encryptedPayload;
-          const [userId, clientId] = this.dismantleSessionId(sessionId);
-          recipients[userId] = recipients[userId] || {};
-          recipients[userId][clientId] = encrypted;
-        });
-      }
+        if (payloads) {
+          payloads.forEach((payload: SessionPayloadBundle) => {
+            const sessionId: string = payload.sessionId;
+            const encrypted: string = payload.encryptedPayload;
+            const [userId, clientId] = this.dismantleSessionId(sessionId);
+            recipients[userId] = recipients[userId] || {};
+            recipients[userId][clientId] = encrypted;
+          });
+        }
 
-      return recipients;
-    });
+        return recipients;
+      });
   }
 
   private encryptPayloadForSession(sessionId: string,
                                    typedArray: Uint8Array,
-                                   encodedPreKey: string): Promise<SessionPayloadBundle> {
+                                   encodedPreKey: string): Promise<string> {
     const decodedPreKeyBundle: Uint8Array = Decoder.fromBase64(encodedPreKey).asBytes;
     return this.cryptobox
       .encrypt(sessionId, typedArray, decodedPreKeyBundle.buffer)
-      .then(encryptedPayload => Encoder.toBase64(encryptedPayload).asString)
-      .catch(error => '💣')
-      .then(encryptedPayload => ({sessionId, encryptedPayload}));
+      .then(encryptedPayload => Encoder.toBase64(encryptedPayload).asString);
   }
 }
