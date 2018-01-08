f3fdebbf946e0ef857c9873118d556afdb61af99
Florian Keller <florian.keller@wire.com>
2018-01-08 16:56:36 +0100

    Improvements

--- packages/cryptobox/webpack.config.js
+++ packages/cryptobox/webpack.config.js
@@ -1,35 +1,34 @@
 const pkg = require('./package.json');
 const webpack = require('webpack');
+console.log('name', pkg.name);
 
 module.exports = {
   devtool: 'source-map',
   entry: {
-    filename: `${__dirname}/dist/commonjs/${pkg.name}.js`
+    filename: `${__dirname}/dist/commonjs/${pkg.name}.js`,
+  },
+  externals: {
+    bazinga64: true,
+    dexie: 'Dexie',
+    'fs-extra': false,
+    logdown: 'Logdown',
+    'wire-webapp-lru-cache': 'LRUCache',
+    'wire-webapp-proteus': 'Proteus',
+  },
+  node: {
+    fs: 'empty',
+    path: 'empty',
   },
   output: {
     filename: `${pkg.name}.js`,
     library: 'cryptobox',
-    path: `${__dirname}/dist/window`
+    path: `${__dirname}/dist/window`,
   },
-  externals: {
-    'bazinga64': true,
-    'dexie': 'Dexie',
-    'logdown': 'Logdown',
-    'fs-extra': false,
-    'wire-webapp-lru-cache': 'LRUCache',
-    'wire-webapp-proteus': 'Proteus'
-  },
-  node: {
-    fs: 'empty',
-    path: 'empty'
-  },
-  plugins: [
-    new webpack.BannerPlugin(`${pkg.name} v${pkg.version}`)
-  ],
   performance: {
+    hints: 'warning',
     maxAssetSize: 100,
     maxEntrypointSize: 300,
-    hints: 'warning'
   },
-  target: 'web'
+  plugins: [new webpack.BannerPlugin(`${pkg.name} v${pkg.version}`)],
+  target: 'web',
 };
