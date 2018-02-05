10f6f5aa4f9906ac547daae6acd3996374de9fcb
Benny Neugebauer <bn@bennyn.de>
2018-02-05 18:55:47 +0100

    Handling PreKeyStore

--- packages/cryptobox/src/main/ts/store/ReadOnlyStore.ts
+++ packages/cryptobox/src/main/ts/store/ReadOnlyStore.ts
@@ -6,19 +6,21 @@ import {CryptoboxStore} from '../store';
  * This store holds IDs of PreKeys which should be deleted.
  */
 class ReadOnlyStore extends ProteusSession.PreKeyStore {
+  private removePrekeys: Array<number> = [];
+
   constructor(private store: CryptoboxStore) {
     super();
-    this.prekeys = [];
+    super.prekeys = [];
   }
 
   /**
-   * Releases PreKeys from list. Called when PreKeys have been deleted.
+   * Releases PreKeys from deletion list. Called when PreKeys have been deleted.
    */
   public release_prekeys(deletedPreKeyIds: Array<number>): void {
     deletedPreKeyIds.forEach((id: number) => {
-      const index: number = this.prekeys.indexOf(id);
+      const index: number = this.removePrekeys.indexOf(id);
       if (index > -1) {
-        this.prekeys.splice(index, 1);
+        super.prekeys.splice(index, 1);
       }
     });
   }
@@ -28,7 +30,7 @@ class ReadOnlyStore extends ProteusSession.PreKeyStore {
    * @override
    */
   get_prekey(prekey_id: number): Promise<ProteusKeys.PreKey | undefined> {
-    if (this.prekeys.indexOf(prekey_id) !== -1) {
+    if (this.removePrekeys.indexOf(prekey_id) !== -1) {
       return Promise.reject(new Error(`PreKey "${prekey_id}" not found.`));
     }
     return this.store.load_prekey(prekey_id);
@@ -38,9 +40,9 @@ class ReadOnlyStore extends ProteusSession.PreKeyStore {
    * Saves the PreKey ID which should get deleted.
    * @override
    */
-  remove(prekey_id: number): Promise<number> {
-    this.prekeys.push(prekey_id);
-    return Promise.resolve(prekey_id);
+  remove(prekey_id: number): Promise<void> {
+    this.removePrekeys.push(prekey_id);
+    return Promise.resolve();
   }
 }
 
