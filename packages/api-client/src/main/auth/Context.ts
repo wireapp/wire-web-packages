fa79a48ef631d4879389309d430358cfb23eb0d1
Michael Koppen <michael.koppen@googlemail.com>
2018-02-20 18:37:45 +0100

    refactor(api-client): Database naming based on environment user ID and client type

--- packages/api-client/src/main/auth/Context.ts
+++ packages/api-client/src/main/auth/Context.ts
@@ -16,8 +16,10 @@
 // along with this program. If not, see http://www.gnu.org/licenses/.
 //
 
+import {ClientType} from '../client/';
+
 class Context {
-  constructor(public userID: string, public clientID?: string) {}
+  constructor(public userID: string, public clientID?: string, public clientType?: ClientType) {}
 }
 
 export {Context};
