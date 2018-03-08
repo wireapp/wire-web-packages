1b293b753a45363e878a1d3e5e466ab9588e2417
Florian Keller <florian.keller@wire.com>
2018-03-08 18:22:17 +0100

    chore(cryptobox): Add benchmark demo

---
+++ packages/cryptobox/src/demo/benchmark.js
@@ -0,0 +1,81 @@
+#!/usr/bin/env node
+
+/* eslint: */
+
+const Proteus = require('@wireapp/proteus');
+const {Cryptobox} = require('@wireapp/cryptobox');
+const {MemoryEngine} = require('@wireapp/store-engine');
+
+const sessionWithAlice = 'session-with-alice';
+const sessionWithBob = 'session-with-bob';
+const messageCount = 3000;
+
+const measure = timer => {
+  const [seconds, nanoseconds] = process.hrtime(timer);
+  const NANOSECOND = 1e9;
+  const digits = 3;
+  return (seconds + nanoseconds / NANOSECOND).toFixed(digits);
+};
+
+async function createCryptobox(storeName, amountOfPreKeys = 1) {
+  const engine = new MemoryEngine();
+  await engine.init(storeName);
+  return new Cryptobox(engine, amountOfPreKeys);
+}
+
+async function setup() {
+  const alice = await createCryptobox('alice', 1);
+  await alice.create();
+
+  const bob = await createCryptobox('bob', 1);
+  await bob.create();
+
+  const bobBundle = Proteus.keys.PreKeyBundle.new(
+    bob.identity.public_key,
+    await bob.store.load_prekey(Proteus.keys.PreKey.MAX_PREKEY_ID)
+  );
+  const cipherMessage = await alice.encrypt(sessionWithBob, 'Hello', bobBundle.serialise());
+  await bob.decrypt(sessionWithAlice, cipherMessage);
+
+  return {alice, bob};
+}
+
+async function benchmark() {
+  const {alice, bob} = await setup();
+
+  const numbers = Array(messageCount)
+    .fill(null)
+    .map((value, key) => key + 1);
+
+  process.stdout.write(`Measuring encryption time for ${numbers.length} messages ... `);
+  const encryptionTimer = process.hrtime();
+  const encryptedMessages = await Promise.all(
+    numbers.map(
+      async value => await alice.encrypt(sessionWithBob, `This is a long message with number ${value.toString()}`)
+    )
+  );
+
+  const elapsedEncryption = measure(encryptionTimer);
+  process.stdout.write('Done.\n');
+  console.log(`Execution time: ${elapsedEncryption} seconds.\n`);
+
+  process.stdout.write(`Measuring decryption time for ${numbers.length} messages ... `);
+  const decryptionTimer = process.hrtime();
+  await Promise.all(
+    encryptedMessages.map(async encryptedMessage => await bob.decrypt(sessionWithAlice, encryptedMessage))
+  );
+
+  const elapsedDecryption = measure(decryptionTimer);
+  process.stdout.write('Done.\n');
+  console.log(`Execution time: ${elapsedDecryption} seconds.`);
+}
+
+(async () => {
+  try {
+    console.log('Setting up ...\n');
+
+    await benchmark();
+  } catch (error) {
+    console.error(error.message, error);
+  }
+})();
