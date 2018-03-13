4eca2bdea5606a1ec76aadd2a209b2a1b15d7874
Benny Neugebauer <bn@bennyn.de>
2018-03-13 16:15:20 +0100

    updated

--- packages/cryptobox/src/demo/benchmark.js
+++ packages/cryptobox/src/demo/benchmark.js
@@ -15,6 +15,12 @@ function createSessionId(receiver) {
   return `session-with-${receiver.identity.public_key.fingerprint()}`;
 }
 
+function numbersInArray(count) {
+  return Array(count)
+    .fill(null)
+    .map((value, key) => key + 1);
+}
+
 /** Creates a Cryptobox with an initialized store. */
 async function createCryptobox(storeName, amountOfPreKeys = 1) {
   const engine = new MemoryEngine();
@@ -43,39 +49,35 @@ async function initialSetup() {
 
 /** Runs the test scenario and measures times. */
 async function encryptBeforeDecrypt({alice, bob}, messageCount) {
-  const numbers = Array(messageCount)
-    .fill(null)
-    .map((value, key) => key + 1);
+  const numbers = numbersInArray(messageCount);
 
   // Encryption
-  process.stdout.write(`Measuring encryption time for ${numbers.length} messages ... `);
-  const encryptionTimer = process.hrtime();
+  process.stdout.write(`Measuring encryption time for "${messageCount}" messages ... `);
+  let startTime = process.hrtime();
   const encryptedMessages = await Promise.all(
     numbers.map(
       async value => await alice.encrypt(createSessionId(bob), `This is a long message with number ${value.toString()}`)
     )
   );
-  const elapsedTimeEncryption = getTimeInSeconds(encryptionTimer);
+  let stopTime = getTimeInSeconds(startTime);
   process.stdout.write('Done.\n');
 
-  console.log(`Execution time: ${elapsedTimeEncryption} seconds.\n`);
+  console.log(`Execution time: ${stopTime} seconds.\n`);
 
   // Decryption
-  process.stdout.write(`Measuring decryption time for "${numbers.length}" messages ... `);
-  const decryptionTimer = process.hrtime();
+  process.stdout.write(`Measuring decryption time for "${messageCount}" messages ... `);
+  startTime = process.hrtime();
   await Promise.all(
     encryptedMessages.map(async encryptedMessage => await bob.decrypt(createSessionId(alice), encryptedMessage))
   );
-  const elapsedTimeDecryption = getTimeInSeconds(decryptionTimer);
+  stopTime = getTimeInSeconds(startTime);
   process.stdout.write('Done.\n');
 
-  console.log(`Execution time: ${elapsedTimeDecryption} seconds.`);
+  console.log(`Execution time: ${stopTime} seconds.\n`);
 }
 
 async function pingPong({alice, bob}, messageCount) {
-  const numbers = Array(messageCount)
-    .fill(null)
-    .map((value, key) => key + 1);
+  const numbers = numbersInArray(messageCount);
 
   function toggleActors([a, b]) {
     return [b, a];
@@ -84,9 +86,9 @@ async function pingPong({alice, bob}, messageCount) {
   let actors = toggleActors([alice, bob]);
 
   process.stdout.write(
-    `Measuring encryption with immediate decryption (ping/pong) for "${numbers.length}" messages ... `
+    `Measuring encryption with immediate decryption (ping/pong) for "${messageCount}" messages ... `
   );
-  const decryptionTimer = process.hrtime();
+  const startTime = process.hrtime();
 
   for (const number of numbers) {
     const sender = actors[0];
@@ -98,10 +100,64 @@ async function pingPong({alice, bob}, messageCount) {
     actors = toggleActors(actors);
   }
 
-  const elapsedTimeDecryption = getTimeInSeconds(decryptionTimer);
+  const stopTime = getTimeInSeconds(startTime);
   process.stdout.write('Done.\n');
 
-  console.log(`Execution time: ${elapsedTimeDecryption} seconds.`);
+  console.log(`Execution time: ${stopTime} seconds.\n`);
+}
+
+async function pingPongWithMultipleSessions(messageCount) {
+  const numbers = numbersInArray(messageCount);
+
+  const alice = await createCryptobox('alice', 1);
+  await alice.create();
+
+  const aliceBundle = Proteus.keys.PreKeyBundle.new(
+    alice.identity.public_key,
+    await alice.store.load_prekey(Proteus.keys.PreKey.MAX_PREKEY_ID)
+  );
+
+  const cryptoboxes = [];
+  const encryptions = [];
+
+  process.stdout.write(`Measuring time for creating "${messageCount}" cryptoboxes ... `);
+  let startTime = process.hrtime();
+
+  for (const number of numbers) {
+    const cryptobox = await createCryptobox(`cryptobox-${number}`, 1);
+    await cryptobox.create();
+    cryptoboxes.push(cryptobox);
+  }
+
+  let stopTime = getTimeInSeconds(startTime);
+  process.stdout.write('Done.\n');
+  console.log(`Execution time: ${stopTime} seconds.\n`);
+
+  process.stdout.write(
+    `Measuring time for encrypting "${messageCount}" messages with "${messageCount}" cryptoboxes ... `
+  );
+  startTime = process.hrtime();
+
+  for (const cryptobox of cryptoboxes) {
+    const cipherMessage = await cryptobox.encrypt(createSessionId(alice), 'Hello', aliceBundle.serialise());
+    encryptions.push(cipherMessage);
+  }
+
+  stopTime = getTimeInSeconds(startTime);
+  process.stdout.write('Done.\n');
+  console.log(`Execution time: ${stopTime} seconds.\n`);
+
+  process.stdout.write(`Measuring time for decrypting "${messageCount}" messages in "${messageCount}" sessions ... `);
+  startTime = process.hrtime();
+
+  for (let index = 0; index < encryptions.length; index++) {
+    const encrypted = encryptions[index];
+    await alice.decrypt(`session-${index}`, encrypted);
+  }
+
+  stopTime = getTimeInSeconds(startTime);
+  process.stdout.write('Done.\n');
+  console.log(`Execution time: ${stopTime} seconds.\n`);
 }
 
 (async () => {
@@ -110,6 +166,7 @@ async function pingPong({alice, bob}, messageCount) {
     const amountOfMessages = 3000;
     await encryptBeforeDecrypt(await initialSetup(), amountOfMessages);
     await pingPong(await initialSetup(), amountOfMessages);
+    await pingPongWithMultipleSessions(amountOfMessages);
   } catch (error) {
     console.error(error.message, error);
   }
