5e2226742eb4853b5340360830dc66d34e61dadd
Michael Koppen <michael.koppen@googlemail.com>
2018-05-25 16:28:17 +0200

    feat: Common package

---
+++ packages/common/webpack.config.js
@@ -0,0 +1,27 @@
+const pkg = require('./package.json');
+const webpack = require('webpack');
+
+const projectName = pkg.name.replace('@wireapp/', '');
+
+module.exports = {
+  devtool: 'source-map',
+  entry: {
+    [`${projectName}.test`]: `${__dirname}/src/main/index.test.browser.js`,
+    [projectName]: `${__dirname}/${pkg.main}`,
+  },
+  externals: {
+    dexie: 'Dexie',
+    'fs-extra': '{}',
+  },
+  mode: 'production',
+  node: {
+    path: 'empty',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    library: 'common',
+    path: `${__dirname}/dist`,
+    publicPath: '/',
+  },
+  plugins: [new webpack.BannerPlugin(`${pkg.name} v${pkg.version}`)],
+};
