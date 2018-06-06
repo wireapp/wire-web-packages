b949b76f5d6739a4130a22383fbb1f8700847ad6
Benny Neugebauer <bn@bennyn.de>
2018-06-06 10:55:56 +0200

    Fixed "decoder.bytes" types

--- packages/proteus/src/main/keys/PreKeyBundle.ts
+++ packages/proteus/src/main/keys/PreKeyBundle.ts
@@ -37,7 +37,7 @@ class PreKeyBundle {
   prekey_id: number;
   public_key: PublicKey;
   identity_key: IdentityKey;
-  signature: Uint8Array | null;
+  signature: Uint8Array | null | undefined;
 
   constructor() {
     this.version = -1;
