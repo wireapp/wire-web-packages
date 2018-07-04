2a6aa4445929cc5afc65fdb5272213d5f48128a5
Benny Neugebauer <bn@bennyn.de>
2018-07-04 15:07:53 +0200

    Add "IncomingEvent"

--- packages/core/src/main/Account.smoke-test.node.js
+++ packages/core/src/main/Account.smoke-test.node.js
@@ -24,11 +24,19 @@ const APIClient = require('@wireapp/api-client');
 const {Config} = require('@wireapp/api-client/dist/commonjs/Config');
 const {ClientType} = require('@wireapp/api-client/dist/commonjs/client/');
 const {MemoryEngine} = require('@wireapp/store-engine');
+const {ValidationUtil} = require('@wireapp/commons');
+const logdown = require('logdown');
+
+const logger = logdown('@wireapp/core/main/Account(SmokeTest)', {
+  logger: console,
+  markdown: false,
+});
+logger.state.isEnabled = true;
 
 const CAN_RUN =
   process.env.ALICE_EMAIL && process.env.ALICE_PASSWORD && process.env.BOB_EMAIL && process.env.BOB_PASSWORD;
 
-async function createAccount(email, password) {
+async function getAccount(email, password) {
   const login = {
     clientType: ClientType.TEMPORARY,
     email,
@@ -45,16 +53,31 @@ async function createAccount(email, password) {
 }
 
 fdescribe('Account', () => {
+  let alice;
+  let bob;
+
+  beforeAll(async done => {
+    if (CAN_RUN) {
+      alice = await getAccount(process.env.ALICE_EMAIL, process.env.ALICE_PASSWORD);
+      bob = await getAccount(process.env.BOB_EMAIL, process.env.BOB_PASSWORD);
+    }
+    done();
+  });
+
   describe('Message Sending', () => {
+    beforeAll(async done => {
+      expect(ValidationUtil.isUUIDv4(alice.apiClient.context.userId)).toBe(true);
+      expect(ValidationUtil.isUUIDv4(bob.apiClient.context.userId)).toBe(true);
+      done();
+    });
+
     it('can send and receive messages', async done => {
       if (!CAN_RUN) {
+        logger.warn('Skipping smoke tests because environment variables are not set.');
         return done();
       }
 
-      const alice = createAccount(process.env.ALICE_EMAIL, process.env.ALICE_PASSWORD);
-      expect(alice).toBeDefined();
-
-      done();
+      alice.done();
     });
   });
 });
