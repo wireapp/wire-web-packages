7cfc9660bbabb12595e3b348f15c24e34f4f14d7
Benny Neugebauer <bn@bennyn.de>
2018-07-13 12:39:58 +0200

    Update BackendErrorMapper.ts

--- packages/api-client/src/main/http/BackendErrorMapper.ts
+++ packages/api-client/src/main/http/BackendErrorMapper.ts
@@ -82,9 +82,9 @@ class BackendErrorMapper {
       if (mappedError) {
         return mappedError;
       }
-      return new BackendError(error.message, error.label, error.code);
+      return error;
     } catch (mappingError) {
-      return new BackendError(error.message, error.label, error.code);
+      return error;
     }
   }
 }
