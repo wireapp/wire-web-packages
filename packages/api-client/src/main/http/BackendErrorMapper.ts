7bb2f461c6f92426f0144a9a214ad465fc3b1f98
Florian Keller <florian.keller@wire.com>
2018-08-10 16:56:05 +0200

    Remove api-client refactoring

--- packages/api-client/src/main/http/BackendErrorMapper.ts
+++ packages/api-client/src/main/http/BackendErrorMapper.ts
@@ -22,7 +22,6 @@ import {
   IdentifierExistsError,
   InvalidCredentialsError,
   LoginTooFrequentError,
-  MissingCookieError,
   SuspendedAccountError,
   TokenExpiredError,
 } from '../auth';
@@ -58,7 +57,6 @@ class BackendErrorMapper {
             'Authentication failed because of invalid credentials.'
           ),
           ['Token expired']: new TokenExpiredError('Authentication failed because the token is expired.'),
-          ['Missing cookie']: new MissingCookieError('Authentication failed because the cookie is missing.'),
         },
         [BackendErrorLabel.NOT_CONNECTED]: {
           ['Users are not connected']: new UnconnectedUserError('Users are not connected.'),
