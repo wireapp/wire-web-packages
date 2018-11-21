d546d27a0e9c1eb43cd45f074ea5a18c4c73cff2
Benny Neugebauer <bn@bennyn.de>
2018-11-21 18:15:51 +0100

    TODO

--- packages/api-client/src/main/APIClient.test.node.js
+++ packages/api-client/src/main/APIClient.test.node.js
@@ -160,7 +160,7 @@ describe('APIClient', () => {
         .catch(done.fail);
     });
 
-    it('refreshes an access token when it becomes invalid', done => {
+    fit('refreshes an access token when it becomes invalid', done => {
       nock(baseURL)
         .get(UserAPI.URL.USERS)
         .query({handles: 'webappbot'})
