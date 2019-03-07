eaeed05f58ca8c3abf3c33b279c4528fe4979c4e
Michael Koppen <michael.koppen@googlemail.com>
2019-03-07 13:55:32 +0100

    chore: sort

--- packages/api-client/src/main/team/payment/PaymentBillingData.ts
+++ packages/api-client/src/main/team/payment/PaymentBillingData.ts
@@ -22,8 +22,8 @@ interface PaymentBillingData {
   city: string;
   company: string;
   country: string;
-  firstname: string;
   currency?: SupportedCurrency;
+  firstname: string;
   lastname: string;
   state?: string;
   street: string;
