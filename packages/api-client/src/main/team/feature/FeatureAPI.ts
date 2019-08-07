3ad267bb053be5bcfb13a8a459aa4bc65bce0a48
Michael Koppen <michael.koppen@googlemail.com>
2019-08-07 12:42:37 +0200

    feat(api-client): Feature API

---
+++ packages/api-client/src/main/team/feature/FeatureAPI.ts
@@ -0,0 +1,53 @@
+/*
+ * Wire
+ * Copyright (C) 2019 Wire Swiss GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ *
+ */
+
+import {AxiosRequestConfig} from 'axios';
+
+import {HttpClient} from '../../http';
+
+export class FeatureAPI {
+  constructor(private readonly client: HttpClient) {}
+
+  static URL = {
+    FEATURES: 'features',
+    LEGAL_HOLD: 'legalhold',
+    SSO: 'sso',
+    TEAMS: '/teams',
+  };
+
+  public async getIdentityProviderFeatureFlag(teamId: string): Promise<{}> {
+    const config: AxiosRequestConfig = {
+      method: 'get',
+      url: `${FeatureAPI.URL.TEAMS}/${teamId}/${FeatureAPI.URL.FEATURES}/${FeatureAPI.URL.SSO}`,
+    };
+
+    const response = await this.client.sendJSON<{}>(config);
+    return response.data;
+  }
+
+  public async getLegalHoldFeatureFlag(teamId: string): Promise<{}> {
+    const config: AxiosRequestConfig = {
+      method: 'get',
+      url: `${FeatureAPI.URL.TEAMS}/${teamId}/${FeatureAPI.URL.FEATURES}/${FeatureAPI.URL.LEGAL_HOLD}`,
+    };
+
+    const response = await this.client.sendJSON<{}>(config);
+    return response.data;
+  }
+}
