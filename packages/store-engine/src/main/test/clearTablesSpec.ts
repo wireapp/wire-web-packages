b156af05d5364d0f60080fc2c6d8c0cccc16ee8b
Florian Keller <florian.keller@wire.com>
2019-12-10 12:49:41 +0100

    add tests

---
+++ packages/store-engine/src/main/test/clearTablesSpec.ts
@@ -0,0 +1,42 @@
+/*
+ * Wire
+ * Copyright (C) 2019 Wire Swiss GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ *
+ */
+
+import {CRUDEngine} from '../engine';
+
+const TABLE_NAME_ONE = 'the-simpsons';
+const TABLE_NAME_TWO = 'spongebob';
+
+export const clearTablesSpec = {
+  'deletes all tables': async (
+    engine: CRUDEngine,
+    initEngine: (shouldCreateNewEngine?: boolean) => Promise<CRUDEngine>,
+  ) => {
+    engine = await initEngine(false);
+
+    await engine.create(TABLE_NAME_ONE, 'one', {name: 'Alpha'});
+    await engine.create(TABLE_NAME_TWO, 'two', {name: 'Beta'});
+    let keys = await engine.readAll(TABLE_NAME_ONE);
+    expect(keys.length).toBe(1);
+
+    await engine.clearTables();
+
+    keys = await engine.readAll(TABLE_NAME_ONE);
+    expect(keys.length).toBe(0);
+  },
+};
