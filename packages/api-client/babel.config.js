4c4a2cb0c64a5044d03500f95cec9a3d75a694e9
Benny Neugebauer <bn@bennyn.de>
2019-12-20 17:23:48 +0100

    fixed browser test setup

--- packages/api-client/babel.config.js
+++ packages/api-client/babel.config.js
@@ -1,4 +1,8 @@
 module.exports = {
-  plugins: [],
+  plugins: [
+    '@babel/plugin-proposal-optional-chaining',
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-proposal-object-rest-spread',
+  ],
   presets: ['@babel/preset-react', '@babel/preset-typescript'],
 };
