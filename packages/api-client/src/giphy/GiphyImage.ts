c15d358e6ef8692be14e72597074d91a8d0959b7
Michael Koppen <michael.koppen@googlemail.com>
2020-01-09 13:24:12 +0100

    chore: Bring back old props

--- packages/api-client/src/giphy/GiphyImage.ts
+++ packages/api-client/src/giphy/GiphyImage.ts
@@ -38,7 +38,28 @@ export interface ImageWebPMeta {
 }
 
 export interface GiphyImage {
+  caption: string;
+  fixed_height_downsampled_height: string;
+  fixed_height_downsampled_url: string;
+  fixed_height_downsampled_width: string;
+  fixed_height_small_height: string;
+  fixed_height_small_still_url: string;
+  fixed_height_small_url: string;
+  fixed_height_small_width: string;
+  fixed_width_downsampled_height: string;
+  fixed_width_downsampled_url: string;
+  fixed_width_downsampled_width: string;
+  fixed_width_small_height: string;
+  fixed_width_small_still_url: string;
+  fixed_width_small_url: string;
+  fixed_width_small_width: string;
   id: string;
+  image_frames: string;
+  image_height: string;
+  image_mp4_url: string;
+  image_original_url: string;
+  image_url: string;
+  image_width: string;
   images: {
     downsized: ImageDimensions & ImageGifMeta;
     downsized_large: ImageDimensions & ImageGifMeta;
