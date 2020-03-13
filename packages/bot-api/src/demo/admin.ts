3c761f0b02a9c743391f14b4c2f1300b79964f9f
Benny Neugebauer <bn@bennyn.de>
2020-03-13 10:24:20 +0100

    style fix

--- packages/bot-api/src/demo/admin.ts
+++ packages/bot-api/src/demo/admin.ts
@@ -1,17 +1,12 @@
-import "dotenv-defaults/config";
+import 'dotenv-defaults/config';
 import {Bot} from '../Bot';
 
 const {CONVERSATION, EMAIL, PASSWORD, USER_ID} = process.env;
 
 (async () => {
-  try {
-    const bot = new Bot({email: EMAIL!, password: PASSWORD!});
-    await bot.start();
-    await bot.sendText(CONVERSATION!, `Promoting user "${USER_ID}" to admin role.`);
-    await bot.setAdminRole(CONVERSATION!, USER_ID!);
-    process.exit(0);
-  } catch (error) {
-    console.error(error);
-    process.exit(1);
-  }
-})();
+  const bot = new Bot({email: EMAIL!, password: PASSWORD!});
+  await bot.start();
+  await bot.sendText(CONVERSATION!, `Promoting user "${USER_ID}" to admin role.`);
+  await bot.setAdminRole(CONVERSATION!, USER_ID!);
+  process.exit(0);
+})().catch(error => console.error(error));
