7c2dd8c77214c68f5170cc1571f2d9c0776343f3
Florian Keller <florian.keller@wire.com>
2020-04-15 17:10:13 +0200

    remove left-over stuff

--- packages/proteus/src/main/keys/IdentityKeyPair.ts
+++ packages/proteus/src/main/keys/IdentityKeyPair.ts
@@ -34,8 +34,6 @@ export class IdentityKeyPair {
     this.public_key = public_key;
     this.secret_key = secret_key;
     this.version = version;
-
-    throw new Error('Using the constructor is not allowed');
   }
 
   static async new(): Promise<IdentityKeyPair> {
