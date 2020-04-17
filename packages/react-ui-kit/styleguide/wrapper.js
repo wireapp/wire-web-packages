d790fd4ede9ee09f3ac7562c47d19683198e2086
bardiawire <bardia@wire.com>
2020-04-17 14:06:09 +0430

    docs: extract children in styleguide wrapper

--- packages/react-ui-kit/styleguide/wrapper.js
+++ packages/react-ui-kit/styleguide/wrapper.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import {StyledApp, THEME_ID} from '@wireapp/react-ui-kit';
 
-function Wrapper(props) {
+function Wrapper({children}) {
   return (
     <div>
       <StyledApp themeId={THEME_ID.LIGHT}>
-        <div style={{padding: 16}}>{props.children}</div>
+        <div style={{padding: 16}}>{children}</div>
       </StyledApp>
     </div>
   );
