0d14298762f83e3430add722444d4f80eac408e2
Florian Imdahl <florian@wire.com>
2021-02-02 14:12:01 +0100

    add comment

--- packages/api-client/src/user/data/UserConnectionData.ts
+++ packages/api-client/src/user/data/UserConnectionData.ts
@@ -21,6 +21,7 @@ import type {Connection} from '../../connection';
 
 export interface UserConnectionData {
   connection: Connection;
+  /** For some statuses (in `connection.status`) no user field is provided. */
   user?: {
     name: string;
   };
