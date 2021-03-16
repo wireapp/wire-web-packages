fdf8ae79e29a1a97e5ceb4f16a1e5174f98aa84b
Michael Koppen <michael.koppen@googlemail.com>
2021-03-16 14:38:42 +0100

    feat(react-ui-kit): GridIcon

---
+++ packages/react-ui-kit/src/Icon/GridIcon.tsx
@@ -0,0 +1,29 @@
+/*
+ * Wire
+ * Copyright (C) 2021 Wire Swiss GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ *
+ */
+
+/** @jsx jsx */
+import {jsx} from '@emotion/core';
+
+import {SVGIcon, SVGIconProps} from './SVGIcon';
+
+export const GridIcon = ({...props}: SVGIconProps) => (
+  <SVGIcon realWidth={16} realHeight={16} {...props}>
+    <path d="M2 10a2 2 0 10.001-3.999A2 2 0 002 10zm12 0a2 2 0 10.001-3.999A2 2 0 0014 10zM2 12a2 2 0 11.001 3.999A2 2 0 012 12zm12 0a2 2 0 11.001 3.999A2 2 0 0114 12zm-6 0a2 2 0 11.001 3.999A2 2 0 018 12zm0-6a2 2 0 11.001 3.999A2 2 0 018 6zM2 0a2 2 0 11.001 3.999A2 2 0 012 0zm12 0a2 2 0 11.001 3.999A2 2 0 0114 0zM8 0a2 2 0 11.001 3.999A2 2 0 018 0z" />
+  </SVGIcon>
+);
