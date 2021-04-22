877c4c76536f4f60e4adcab41c1a9777d96aed87
Florian Imdahl <florian@wire.com>
2021-04-22 15:53:36 +0200

    fix headers

--- packages/react-ui-kit/styleguide.config.js
+++ packages/react-ui-kit/styleguide.config.js
@@ -15,7 +15,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see http://www.gnu.org/licenses/.
  *
-*/
+ */
 
 /* eslint-disable sort-keys-fix/sort-keys-fix */
 const fs = require('fs');
