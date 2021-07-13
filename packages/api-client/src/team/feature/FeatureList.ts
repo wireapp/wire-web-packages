09743925717ea1484ca852f0e3bd51f8bc68efd1
Michael Koppen <michael.koppen@googlemail.com>
2021-07-13 15:07:04 +0200

    fix: Feature type

--- packages/api-client/src/team/feature/FeatureList.ts
+++ packages/api-client/src/team/feature/FeatureList.ts
@@ -24,6 +24,7 @@ import type {
   FeatureLegalhold,
   FeatureConferenceCalling,
   FeatureVideoCalling,
+  FeatureFileSharing,
 } from './Feature';
 
 export enum FEATURE_KEY {
@@ -43,7 +44,7 @@ export type FeatureList = {
   [FEATURE_KEY.DIGITAL_SIGNATURES]?: FeatureDigitalSignature;
   [FEATURE_KEY.CONFERENCE_CALLING]?: FeatureConferenceCalling;
   [FEATURE_KEY.VIDEO_CALLING]?: FeatureVideoCalling;
-  [FEATURE_KEY.FILE_SHARING]?: FeatureVideoCalling;
+  [FEATURE_KEY.FILE_SHARING]?: FeatureFileSharing;
   [FEATURE_KEY.LEGALHOLD]?: FeatureLegalhold;
   [FEATURE_KEY.SEARCH_VISIBILITY]?: FeatureWithoutConfig;
   [FEATURE_KEY.SSO]?: FeatureWithoutConfig;
