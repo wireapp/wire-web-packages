ec8b0a75d4ee60134ff7d2696728c1caedf9f212
PatrykBuniX <patrykbunix@gmail.com>
2022-11-21 16:24:55 +0100

    test: add cases for extractEncryptedAndMissingFromBatchedPayload

--- packages/core/src/messagingProtocols/proteus/ProteusService/ProteusService.test.ts
+++ packages/core/src/messagingProtocols/proteus/ProteusService/ProteusService.test.ts
@@ -29,6 +29,7 @@ import {Decoder} from 'bazinga64';
 import {preKeyBundleToUserClients} from '../../../util/preKeyBundleToUserClients';
 
 import {buildProteusService} from './ProteusService.mocks';
+import {UserPreKeyBundleMap} from '@wireapp/api-client/lib/user';
 
 jest.mock('../Utility/Recipients', () => ({
   ...jest.requireActual('../Utility/Recipients'),
@@ -392,6 +393,85 @@ describe('ProteusService', () => {
           [secondUserID]: [secondUserClient],
         },
       });
+    });
+  });
+
+  describe('extractEncryptedAndMissingFromBatchedPayload', () => {
+    it('properly extracts missing and encrypted from payload (without missing)', async () => {
+      const [proteusService] = buildProteusService();
+
+      const domain = 'staging.zinfra.io';
+
+      const firstUserID = 'bc0c99f1-49a5-4ad2-889a-62885af37088';
+      const firstUserClient1 = '5e80ea7886680975';
+      const firstUserClient2 = 'be67218b77d02d30';
+
+      const secondUserID = '2bde49aa-bdb5-458f-98cf-7d3552b10916';
+      const secondUserClient = '5bad8cdeddc5a90f';
+
+      const userClients: UserClients = {
+        [firstUserID]: [firstUserClient1, firstUserClient2],
+        [secondUserID]: [secondUserClient],
+      };
+
+      const textPayload = new Uint8Array();
+
+      const batchedEncryptPayload: Map<string, Uint8Array> = new Map(
+        [
+          [firstUserID, firstUserClient1, domain],
+          [firstUserID, firstUserClient2, domain],
+          [secondUserID, secondUserClient, domain],
+        ].map(([u, c, d]) => [proteusService['cryptographyService'].constructSessionId(u, c, d), textPayload]),
+      );
+
+      const {encrypted, missing} = proteusService['extractEncryptedAndMissingFromBatchedPayload'](
+        batchedEncryptPayload,
+        userClients,
+        domain,
+      );
+
+      expect(encrypted).toEqual({
+        [firstUserID]: {[firstUserClient1]: textPayload, [firstUserClient2]: textPayload},
+        [secondUserID]: {[secondUserClient]: textPayload},
+      });
+
+      expect(missing).toEqual({});
+    });
+
+    it('properly extracts missing and encrypted from payload (with missing)', async () => {
+      const [proteusService] = buildProteusService();
+
+      const domain = 'staging.zinfra.io';
+
+      const firstUserID = 'bc0c99f1-49a5-4ad2-889a-62885af37088';
+      const firstUserClient1 = '5e80ea7886680975';
+      const firstUserClient2 = 'be67218b77d02d30';
+
+      const secondUserID = '2bde49aa-bdb5-458f-98cf-7d3552b10916';
+      const secondUserClient = '5bad8cdeddc5a90f';
+
+      const userClients: UserClients = {
+        [firstUserID]: [firstUserClient1, firstUserClient2],
+        [secondUserID]: [secondUserClient],
+      };
+
+      const textPayload = new Uint8Array();
+
+      const batchedEncryptPayload: Map<string, Uint8Array> = new Map([
+        [proteusService['cryptographyService'].constructSessionId(firstUserID, firstUserClient1, domain), textPayload],
+      ]);
+
+      const {encrypted, missing} = proteusService['extractEncryptedAndMissingFromBatchedPayload'](
+        batchedEncryptPayload,
+        userClients,
+        domain,
+      );
+
+      expect(encrypted).toEqual({
+        [firstUserID]: {[firstUserClient1]: textPayload},
+      });
+
+      expect(missing).toEqual({[firstUserID]: [firstUserClient2], [secondUserID]: [secondUserClient]});
     });
   });
 
