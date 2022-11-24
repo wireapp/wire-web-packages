fef13ba1d90a5685b5ea98bee43edc9aa2552175
Thomas Belin <me@thomasbelin.fr>
2022-11-24 17:37:15 +0100

    fix tests

--- packages/core/src/Account.test.ts
+++ packages/core/src/Account.test.ts
@@ -80,6 +80,8 @@ const waitFor = (assertion: () => void) => {
 describe('Account', () => {
   const CLIENT_ID = '4e37b32f57f6da55';
 
+  // Fix for node 16, crypto.subtle.decrypt has a type problem
+  jest.spyOn(global.crypto.subtle, 'decrypt').mockImplementation();
   const accessTokenData = {
     access_token:
       'iJCRCjc8oROO-dkrkqCXOade997oa8Jhbz6awMUQPBQo80VenWqp_oNvfY6AnU5BxEsdDPOBfBP-uz_b0gAKBQ==.v=1.k=1.d=1498600993.t=a.l=.u=aaf9a833-ef30-4c22-86a0-9adc8a15b3b4.c=15037015562284012115',
