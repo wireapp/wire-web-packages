b4f8e13805821eb0636acd2454843478ddf50514
Thomas Belin <me@thomasbelin.fr>
2022-12-29 15:10:08 +0100

    improve tests

--- packages/core/src/messagingProtocols/proteus/ProteusService/DecryptionErrorGenerator/DecryptionErrorGenerator.test.ts
+++ packages/core/src/messagingProtocols/proteus/ProteusService/DecryptionErrorGenerator/DecryptionErrorGenerator.test.ts
@@ -17,7 +17,7 @@
  *
  */
 
-import {generateDecryptionError} from './DecryptionErrorGenerator';
+import {generateDecryptionError, ProteusErrors} from './DecryptionErrorGenerator';
 
 import {DecryptionError} from '../../../../errors/DecryptionError';
 
@@ -27,6 +27,21 @@ describe('generateDecryptionError', () => {
   it('returns a ProteusError.DecryptError', () => {
     const error = generateDecryptionError(basePayload, new Error());
     expect(error).toBeInstanceOf(DecryptionError);
-    expect(error.message).toBe('Unknown decryption error');
+    expect(error.message).toBe('Failed to decrypt message from user1 (client1) with unkown error');
+    expect(error.code).toBe(ProteusErrors.Unknown);
+  });
+
+  it('handles remote identity changed', () => {
+    const error = generateDecryptionError(basePayload, new Error('RemoteIdentityChanged'));
+    expect(error).toBeInstanceOf(DecryptionError);
+    expect(error.message).toBe('Remote identity of user1 (client1) has changed');
+    expect(error.code).toBe(ProteusErrors.RemoteIdentityChanged);
+  });
+
+  it('handles invalid message', () => {
+    const error = generateDecryptionError(basePayload, new Error('InvalidMessage'));
+    expect(error).toBeInstanceOf(DecryptionError);
+    expect(error.message).toBe('Invalid message from user1 (client1)');
+    expect(error.code).toBe(ProteusErrors.InvalidMessage);
   });
 });
