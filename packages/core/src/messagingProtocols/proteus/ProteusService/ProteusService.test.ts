ca4c63f5f9cc1f68fc651d3d155831d811ff66fc
Thomas Belin <me@thomasbelin.fr>
2023-03-02 10:39:43 +0100

    fix tests

--- packages/core/src/messagingProtocols/proteus/ProteusService/ProteusService.test.ts
+++ packages/core/src/messagingProtocols/proteus/ProteusService/ProteusService.test.ts
@@ -328,18 +328,20 @@ describe('ProteusService', () => {
       ]);
 
       jest.spyOn(services.apiClient.api.user, 'postMultiPreKeyBundles').mockResolvedValue({
-        [domain]: {
-          [firstUser.id.id]: {
-            [firstUser.clients.first]: null,
-            [firstUser.clients.second]: {
-              id: 123,
-              key: 'pQABARhIAqEAWCCaJpFa9c626ORmjj1aV6OnOYgmTjfoiE3ynOfNfGAOmgOhAKEAWCD60VMzRrLfO+1GSjgyhnVp2N7L58DM+eeJhZJi1tBLfQT2',
+        qualified_user_client_prekeys: {
+          [domain]: {
+            [firstUser.id.id]: {
+              [firstUser.clients.first]: null,
+              [firstUser.clients.second]: {
+                id: 123,
+                key: 'pQABARhIAqEAWCCaJpFa9c626ORmjj1aV6OnOYgmTjfoiE3ynOfNfGAOmgOhAKEAWCD60VMzRrLfO+1GSjgyhnVp2N7L58DM+eeJhZJi1tBLfQT2',
+              },
             },
-          },
-          [secondUser.id.id]: {
-            [secondUser.clients.first]: {
-              id: 123,
-              key: 'pQABARhIAqEAWCCaJpFa9c626ORmjj1aV6OnOYgmTjfoiE3ynOfNfGAOmgOhAKEAWCD60VMzRrLfO+1GSjgyhnVp2N7L58DM+eeJhZJi1tBLfQT2',
+            [secondUser.id.id]: {
+              [secondUser.clients.first]: {
+                id: 123,
+                key: 'pQABARhIAqEAWCCaJpFa9c626ORmjj1aV6OnOYgmTjfoiE3ynOfNfGAOmgOhAKEAWCD60VMzRrLfO+1GSjgyhnVp2N7L58DM+eeJhZJi1tBLfQT2',
+              },
             },
           },
         },
