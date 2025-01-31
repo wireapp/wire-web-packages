2a67412a26512e07e7c95fe1d15267db77b3fc20
Olaf Sulich <olafsulich@gmail.com>
2025-01-31 11:40:33 +0100

    feat: add stories

--- packages/react-ui-kit/.storybook/preview.tsx
+++ packages/react-ui-kit/.storybook/preview.tsx
@@ -4,20 +4,34 @@ import {withThemeFromJSXProvider} from '@storybook/addon-themes';
 import {GlobalStyle} from '../src/GlobalStyle';
 import {THEME_ID, ThemeProvider, themes} from '../src/Layout';
 
-export const decorators = [
-  withThemeFromJSXProvider({
-    themes: {
-      light: themes[THEME_ID.LIGHT],
-      dark: themes[THEME_ID.DARK],
-    },
-    defaultTheme: 'light',
-    Provider: ThemeProvider,
-    GlobalStyles: GlobalStyle,
-  }),
-];
+const availableThemes = {
+  light: themes[THEME_ID.LIGHT],
+  dark: themes[THEME_ID.DARK],
+};
 
 const preview: Preview = {
+  decorators: [
+    withThemeFromJSXProvider({
+      themes: availableThemes,
+      defaultTheme: 'light',
+      Provider: ThemeProvider,
+      GlobalStyles: GlobalStyle,
+    }),
+  ],
   parameters: {
+    themes: {
+      default: 'light',
+      list: [
+        {name: 'light', class: '', color: '#ffffff'},
+        {name: 'dark', class: '', color: '#000000'},
+      ],
+      disable: ({viewMode}) => viewMode === 'docs',
+    },
+    docs: {
+      story: {
+        inline: true,
+      },
+    },
     controls: {
       matchers: {
         color: /(background|color)$/i,
