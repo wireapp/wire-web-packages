8442b20c50aefc8b71a0902414bbfaf8df934766
Olaf Sulich <olafsulich@gmail.com>
2025-05-13 10:00:39 +0200

    feat(react-ui-kit): Breadcrumbs component

---
+++ packages/react-ui-kit/src/Misc/Breadcrumbs/CombainedBreadcrumbs/CombainedBreadcrumbs.tsx
@@ -0,0 +1,45 @@
+/*
+ * Wire
+ * Copyright (C) 2025 Wire Swiss GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ *
+ */
+
+import {buttonStyles} from './CombainedBreadcrumbs.styles';
+
+import {DropdownMenu} from '../../../DropdownMenu';
+
+interface CombainedBreadcrumbsProps {
+  items: Array<{name: string}>;
+}
+
+export const CombainedBreadcrumbs = ({items}: CombainedBreadcrumbsProps) => {
+  return (
+    <DropdownMenu>
+      <DropdownMenu.Trigger asChild>
+        <li>
+          <button type="button" css={buttonStyles}>
+            ...
+          </button>
+        </li>
+      </DropdownMenu.Trigger>
+      <DropdownMenu.Content>
+        {items.map(crumb => (
+          <DropdownMenu.Item key={crumb.name}>{crumb.name}</DropdownMenu.Item>
+        ))}
+      </DropdownMenu.Content>
+    </DropdownMenu>
+  );
+};
