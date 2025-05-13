d2866a57dccc20cd45703cb11b42fc9ac41be3b4
Olaf Sulich <olafsulich@gmail.com>
2025-05-13 09:40:36 +0200

    feat(react-ui-kit): DropdownMenu component

--- packages/react-ui-kit/src/DropdownMenu/index.ts
+++ packages/react-ui-kit/src/DropdownMenu/index.ts
@@ -1,6 +1,6 @@
 /*
  * Wire
- * Copyright (C) 2022 Wire Swiss GmbH
+ * Copyright (C) 2025 Wire Swiss GmbH
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -17,15 +17,4 @@
  *
  */
 
-/// <reference types="@emotion/react/types/css-prop" />
-
-export * from './Form';
-export * from './Icon';
-export * from './Identity';
-export * from './Layout';
-export * from './Menu';
-export * from './Misc';
-export * from './Modal';
-export * from './Text';
-export * from './mediaQueries';
 export * from './DropdownMenu';
