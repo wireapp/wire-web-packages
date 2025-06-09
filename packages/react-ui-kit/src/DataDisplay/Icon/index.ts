c66af02c1ad5a7976e38f35476fa3b5db694f3e6
Olaf Sulich <olafsulich@gmail.com>
2025-06-09 07:46:10 +0200

    chore: reexpoert SVIcon

--- packages/react-ui-kit/src/DataDisplay/Icon/index.ts
+++ packages/react-ui-kit/src/DataDisplay/Icon/index.ts
@@ -1,6 +1,6 @@
 /*
  * Wire
- * Copyright (C) 2018 Wire Swiss GmbH
+ * Copyright (C) 2025 Wire Swiss GmbH
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -17,6 +17,7 @@
  *
  */
 
+export * from './SVGIcon';
 export * from './AddPeopleIcon';
 export * from './ArchiveIcon';
 export * from './ArrowIcon';
