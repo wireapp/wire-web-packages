4dd38b6d84f3f57e30701287e96dfd5cb4e8477e
EnricoSchw <enrico.schwendig@wire.com>
2026-03-17 13:57:35 +0100

    feat: Add new SVG icons, BlurHighIcon, BlurLowIcon, and CircleCloseIcon

---
+++ packages/react-ui-kit/src/DataDisplay/Icon/BlurHighIcon.tsx
@@ -0,0 +1,26 @@
+/*
+ * Wire
+ * Copyright (C) 2019 Wire Swiss GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ *
+ */
+
+import {SVGIcon, SVGIconProps} from './SVGIcon';
+
+export const BlurHighIcon = (props: SVGIconProps) => (
+  <SVGIcon realWidth={23} realHeight={23} {...props}>
+    <path d="M0.530301 11.8433L-2.94902e-05 11.313L11.313 0L11.8433 0.53033L0.530301 11.8433ZM1.87822 13.1913L1.34789 12.6609L12.6609 1.34792L13.1912 1.87825L1.87822 13.1913ZM3.22615 14.5392L2.69582 14.0089L14.0088 2.69584L14.5392 3.22617L3.22615 14.5392ZM4.57407 15.8871L4.04374 15.3568L15.3568 4.04377L15.8871 4.5741L4.57407 15.8871ZM5.92199 17.235L5.39166 16.7047L16.7047 5.39169L17.235 5.92202L5.92199 17.235ZM7.26991 18.583L6.73958 18.0526L18.0526 6.73961L18.5829 7.26994L7.26991 18.583ZM8.61783 19.9309L8.0875 19.4006L19.4005 8.08753L19.9309 8.61786L8.61783 19.9309ZM9.96576 21.2788L9.43543 20.7485L20.7484 9.43546L21.2788 9.96579L9.96576 21.2788ZM11.3137 22.6267L10.7833 22.0964L22.0964 10.7834L22.6267 11.3137L11.3137 22.6267Z" fill="black"></path>
+  </SVGIcon>
+);
---
+++ packages/react-ui-kit/src/DataDisplay/Icon/BlurLowIcon.tsx
@@ -0,0 +1,26 @@
+/*
+ * Wire
+ * Copyright (C) 2019 Wire Swiss GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ *
+ */
+
+import {SVGIcon, SVGIconProps} from './SVGIcon';
+
+export const BlurLowIcon = (props: SVGIconProps) => (
+  <SVGIcon realWidth={23} realHeight={23} {...props}>
+    <path d="M0.342517 11.6555L1.23978e-05 11.313L11.313 -4.06212e-05L11.6555 0.342464L0.342517 11.6555ZM2.46578 13.7787L2.12327 13.4362L13.4363 2.12322L13.7788 2.46573L2.46578 13.7787ZM4.5904 15.902L4.2479 15.5595L15.5609 4.24646L15.9034 4.58897L4.5904 15.902ZM6.71367 18.0252L6.37116 17.6827L17.6842 6.36972L18.0267 6.71223L6.71367 18.0252ZM8.8376 20.1492L8.49509 19.8067L19.8081 8.49366L20.1506 8.83616L8.8376 20.1492ZM10.9609 22.2724L10.6184 21.9299L21.9314 10.6169L22.2739 10.9594L10.9609 22.2724Z" fill="black"></path>
+  </SVGIcon>
+);
